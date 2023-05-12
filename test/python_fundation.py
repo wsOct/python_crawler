988af83f061b67ae800e9bd0460eda299ec5a7f7
wsOct <810702025@qq.com>
2023-05-12 16:11:25 +0800

    python basic

---
+++ test/python_fundation.py
@@ -0,0 +1,29 @@
+# guess number
+# import random
+#
+# num = random.randint(1, 10)  # 随机生成1－10之间的数字
+# answer = int(input('猜个数字: '))  # 将用户输入的字符转成整数
+# if answer > num:
+#     print('猜大了')
+# elif answer < num:
+#     print('猜小了')
+# else:
+#     print('猜对了')
+#
+# print('正确答案是:', num)
+
+
+
+#1+2+3...+100
+# n=1
+# count=0
+# while n<=100:
+#     count=count+n
+#     n=n+1
+# print(count)
+
+# for i in range(1,101):
+#     count+=i
+# print(count)
+#
+# print(sum(range(1,101)))
